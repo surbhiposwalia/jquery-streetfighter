eed0ef579bcbfa8c3b7278705eff3b15f98bbcae
surbhiposwalia <poswalia@ac87a31958e6.ant.amazon.com>
2016-08-22 16:08:20 -0700

    done updating

--- js/app.js
+++ js/app.js
@@ -24,36 +24,36 @@ $(document).ready(function() {
         })
     $(document).keydown(function(event) {
         if (event.which == 88) {
+            playHadouken();
             $('.ryu-still').hide();
             $('.ryu-ready').hide();
             $('.ryu-throwing').hide();
             $('.ryu-cool').show();
+
         }
     });
     $(document).keyup(function(event) {
-
+        if (event.which == 88) {
+//$('#hadouken-sound')[0].pause();
         $('.ryu-still').show();
         $('.ryu-ready').hide();
         $('.ryu-throwing').hide();
         $('.ryu-cool').hide();
 
+}
 
     });
-    //$('.ryu').bind('keypress', function(e) {
-    //  if (e.keyCode == 13) {
-    /*$('.ryu-still').hide();
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').hide();
-            $('.ryu-cool').show();
-         */ // Enter pressed... do anything here...
-    // alert('a WAS PRESSED');
-    //}
-    //});
-
 });
 
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
+}
+function playHard() {
+    $('#hard-sound')[0].volume = 0.5;
+    $('#hard-sound')[0].load();
+    $('#hard-sound')[0].play();
+
+
 }
